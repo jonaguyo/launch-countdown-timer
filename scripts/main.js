267e7215678e8b2163f2050600f0c9fee76d8482
jonaguyo <jonaguyo@gmail.com>
2021-04-01 00:02:59 +0200

    Commit from home

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,42 @@
 updateCountdown();
-//window.setInterval(updateCountdown, 1000);
+window.setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-    let daysElement = document.getElementById("days");
-    let hoursElement = document.getElementById("hours");
-    let minutesElement = document.getElementById("minutes");
-    let secondsElement = document.getElementById("seconds");
-
     let date = new Date();
-    let days = date.getDay();
-    let hours = date.getHours();
+
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-    daysElement.getElementsByClassName("current")[0].innerText = days;
-    hoursElement.getElementsByClassName("current")[0].innerText = hours;
-    minutesElement.getElementsByClassName("current")[0].innerText = minutes;
-    secondsElement.getElementsByClassName("current")[0].innerText = seconds;
-    secondsElement.getElementsByClassName("current")[1].innerText = seconds;
+    let minutesElement = document.getElementById("minutes");
+    let secondsElement = document.getElementById("seconds");
 
-    daysElement.getElementsByClassName("new")[0].innerText = days;
-    hoursElement.getElementsByClassName("new")[0].innerText = hours;
-    minutesElement.getElementsByClassName("new")[0].innerText = minutes;
-    secondsElement.getElementsByClassName("new")[0].innerText = seconds + 1;
-    secondsElement.getElementsByClassName("new")[1].innerText = seconds + 1;
+    updateCounter(minutesElement, minutes);
+    updateCounter(secondsElement, seconds);
+}
+
+function updateCounter(element, newValue) {
+    let currentValue = element.getElementsByClassName("current-value")[0].innerText;
+
+    if(currentValue != newValue) {
+        element.getElementsByClassName("top current")[0].classList.add("no-transition");
+        element.getElementsByClassName("top current")[0].style["background-color"] = "#2a2c41";
+        element.getElementsByClassName("top current")[0].style["transform"] = "rotateX(0deg)";
+        element.getElementsByClassName("bottom new")[0].classList.add("no-transition");
+        element.getElementsByClassName("bottom new")[0].style["background-color"] = "#53567f";
+        element.getElementsByClassName("bottom new")[0].style["transform"] = "rotateX(90deg)";
+
+        element.getElementsByClassName("current-value")[0].innerText = newValue - 1;
+        element.getElementsByClassName("current-value")[1].innerText = newValue;
+        element.getElementsByClassName("new-value")[0].innerText = newValue;
+        element.getElementsByClassName("new-value")[1].innerText = newValue;
+
+        element.getElementsByClassName("top current")[0].offsetHeight;
+        element.getElementsByClassName("top current")[0].classList.remove("no-transition");
+        element.getElementsByClassName("top current")[0].style["background-color"] = "#161722";
+        element.getElementsByClassName("top current")[0].style["transform"] = "rotateX(-90deg)";
+        element.getElementsByClassName("bottom new")[0].offsetHeight;
+        element.getElementsByClassName("bottom new")[0].classList.remove("no-transition");
+        element.getElementsByClassName("bottom new")[0].style["background-color"] = "#343650";
+        element.getElementsByClassName("bottom new")[0].style["transform"] = "rotateX(0deg)";
+    }
 }
